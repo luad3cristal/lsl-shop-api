d48ba9f20b5c1adebccf5ef43d8299ad3991077a
Luana Lima <luanalima.2006@hotmail.com>
2026-03-08 02:31:15 -0300

    initial commit

---
+++ product-shop-service/src/main/java/com/lsl_shop/shop/ProductShopServiceApplication.java
@@ -0,0 +1,15 @@
+package com.lsl_shop.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ProductShopServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductShopServiceApplication.class, args);
+	}
+
+}
---
+++ product-shop-service/src/test/java/com/lsl_shop/shop/ProductShopServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsl_shop.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductShopServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
