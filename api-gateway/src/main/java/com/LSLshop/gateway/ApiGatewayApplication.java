b1b54e3839bbe6abd6635c35727e7e4d607dabdb
Luana Lima <luanalima.2006@hotmail.com>
2026-03-31 00:26:07 -0300

    refactor: change groupId to use com.LSLshop

--- api-gateway/src/main/java/com/LSLshop/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/LSLshop/gateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.lsl_shop.gateway;
+package com.LSLshop.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- auth-user-service/src/main/java/com/LSLshop/auth/AuthUserServiceApplication.java
+++ auth-user-service/src/main/java/com/LSLshop/auth/AuthUserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.lsl_shop.shop;
+package com.LSLshop.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ProductShopServiceApplication {
+public class AuthUserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductShopServiceApplication.class, args);
+		SpringApplication.run(AuthUserServiceApplication.class, args);
 	}
 
 }
