ea2d332a30a7c1f2544813f26eb442dede7e8241
Luana Lima <luanalima.2006@hotmail.com>
2026-08-11 00:58:46 -0300

    feat: create UserRepository and define its methods

---
+++ auth-user-service/src/main/java/com/LSLshop/auth/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.lslshop.auth.repository;
+
+import com.lslshop.auth.models.User;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByEmail (String email);
+  Optional<User> findByEmailAndActiveTrue(String email);
+  List<User> findAllByActiveTrue ();
+
+  boolean existsByEmail (String email);
+}
